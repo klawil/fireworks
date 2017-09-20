ff82ae14657ddde53cce08f3f073f9c2ab11315f
William Klausmeyer <willyk95@gmail.com>
2017-09-20 09:50:18 -0500

    Update the schema for the users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,17 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('password')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('phone')->nullable();
+            $table->foreign('parent_user')
+              ->references('id')->on('users')
+              ->default(1)
+              ->onDelete('cascades');
             $table->rememberToken();
             $table->timestamps();
         });
