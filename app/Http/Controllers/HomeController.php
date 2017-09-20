21c5ee4bb37170cfa5e7b624b596411bbc679c99
William Klausmeyer <willyk95@gmail.com>
2017-09-20 15:53:55 -0500

    Make the home page show past and upcoming shows

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,23 +6,43 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $Request)
+  {
+    $UpcomingShows = $Request
+      ->user()
+      ->shows()
+      ->where('planned_date', '>=', \Carbon\Carbon::today())
+      ->orderBy('planned_date', 'asc')
+      ->get();
+
+    $PastShows = $Request
+      ->user()
+      ->shows()
+      ->where('planned_date', '<', \Carbon\Carbon::today())
+      ->orderBy('planned_date', 'desc')
+      ->get();
+
+    return view(
+      'home',
+      [
+        'upcoming' => $UpcomingShows,
+        'past' => $PastShows,
+      ]
+    );
+  }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,72 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">Upcoming Shows</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+        <div class="panel-body">
+          @if(count($upcoming) === 0)
+          <h4>There isn't anything here! Try creating a show</h4>
+          @else
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Name</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($upcoming as $show)
+              <tr>
+                <th>{{ $show->planned_date }}</th>
+                <th>{{ $show->name }}</th>
+                <th>
+                  <a class="btn btn-success" href="/">View Show</a>
+                </th>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+          @endif
         </div>
+      </div>
     </div>
+
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">Past Shows</div>
+
+        <div class="panel-body">
+          @if(count($past) === 0)
+          <h4>There isn't anything here! Try creating a show</h4>
+          @else
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Name</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($past as $show)
+              <tr>
+                <th>{{ $show->planned_date }}</th>
+                <th>{{ $show->name }}</th>
+                <th>
+                  <a class="btn btn-success" href="/">View Show</a>
+                </th>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+          @endif
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
