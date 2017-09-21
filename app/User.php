285c300cc4c3224e10b31aa144c3d25a429602b7
William Klausmeyer <willyk95@gmail.com>
2017-09-21 09:54:18 -0500

    Add helpers for user-user permissions

--- app/User.php
+++ app/User.php
@@ -34,4 +34,26 @@ class User extends Authenticatable
   public function shows() {
     return $this->belongsToMany('App\Show', 'show_roles', 'user_id', 'show_id')->withTimestamps();
   }
+
+  /**
+   * The users that this user can see
+   * @return User The users that this user can see
+   */
+  public function userCanSee() {
+    return $this
+      ->belongsToMany('App\User', 'user_user', 'viewer_id', 'user_id')
+      ->withPivot('type')
+      ->withTimestamps();
+  }
+
+  /**
+   * The users that can see this user
+   * @return User The users that can see this user
+   */
+  public function canSeeUser() {
+    return $this
+      ->belongsToMany('App\User', 'user_user', 'user_id', 'viewer_id')
+      ->withPivot('type')
+      ->withTimestamps();
+  }
 }
