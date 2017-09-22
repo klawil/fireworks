9d553328a017ca1e7f3d08a999ce66ebf744b6e0
William Klausmeyer <willyk95@gmail.com>
2017-09-22 12:47:15 -0500

    Update the style for the user model

--- app/User.php
+++ app/User.php
@@ -63,6 +63,7 @@ class User extends Authenticatable
    * @return App\License The licenses the user has
    */
   public function licenses() {
-    return $this->hasMany('App\License');
+    return $this
+      ->hasMany('App\License');
   }
 }
