896313d09bc1dd650b982ebd86325df89defbace
William Klausmeyer <willyk95@gmail.com>
2017-09-23 10:15:09 -0500

    Add the ability to show messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,12 @@
       </div>
     </nav>
 
+    @if(Session::has('message'))
+    <div class="container">
+      <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+    </div>
+    @endif
+
     @yield('content')
   </div>
 
