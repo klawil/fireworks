b0560fa0e4b4ccc38173885a81258b4bc45663df
William Klausmeyer <willyk95@gmail.com>
2017-09-25 15:50:14 -0500

    Remove the unused File controller methods

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -9,27 +9,6 @@ use Illuminate\Http\Request;
 class FileController extends Controller
 {
   /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    //
-  }
-
-  /**
    * Display the specified resource.
    *
    * @param  \App\File  $file
@@ -44,19 +23,5 @@ class FileController extends Controller
     return \Response(Storage::get($file->storage_name), 200)
       ->header('Content-Type', $file->mimetype)
       ->header('Content-Disposition', 'inline; filename="' . $file->file_name . '"');
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\File  $file
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(File $file)
-  {
-    // Authorize the user
-    $this->authorize('delete', $file);
-
-    //
   }
 }
