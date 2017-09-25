e06f7b0bb00e054e0706c3ee25c0cd71fe92e12f
William Klausmeyer <willyk95@gmail.com>
2017-09-25 17:00:14 -0500

    Add a view to display a user profile

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">{{ $user->last_name }}, {{ $user->first_name }}</div>
+
+        <div class="panel-body">
+          <table class="table table-striped">
+            <tbody>
+              <tr>
+                <th>First Name</th>
+                <td>{{ $user->first_name }}</td>
+              </tr>
+              <tr>
+                <th>Last Name</th>
+                <td>{{ $user->last_name }}</td>
+              </tr>
+              @can('view', $user)
+              <tr>
+                <th>Phone Number</th>
+                <td>{{ $user->phone or 'N/A' }}</td>
+              </tr>
+              <tr>
+                <th>Email</th>
+                <td>{{ $user->email or 'N/A' }}</td>
+              </tr>
+              <tr>
+                <th>City/State</th>
+                <td>{{ $user->city && $user->state ? $user->city . ', ' . $user->state : 'N/A' }}</td>
+              </tr>
+              @endcan
+              @can('update', $user)
+              <tr>
+                <td colspan="2">
+                  <a href="" class="btn btn-primary btn-block">Edit User</a>
+                </td>
+              </tr>
+              @endcan
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
