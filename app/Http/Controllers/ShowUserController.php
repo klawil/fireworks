0f51f100878a1b27ee7bd348bd1af58608879e8a
William Klausmeyer <willyk95@gmail.com>
2017-09-27 14:51:28 -0500

    Add the functionality for show-user relationships

--- app/Http/Controllers/ShowUserController.php
+++ app/Http/Controllers/ShowUserController.php
@@ -3,83 +3,216 @@
 namespace App\Http\Controllers;
 
 use App\ShowUser;
+use App\Show;
+use App\User;
 use Illuminate\Http\Request;
 
 class ShowUserController extends Controller
 {
   /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
+   * The validation rules for a user and show
    */
-  public function index()
-  {
-    //
-  }
+  private $rules = [
+    'is_owner' => 'required|boolean',
+    'is_driver' => 'required|boolean',
+    'is_shooter' => 'required|boolean',
+    'is_assistant' => 'required|boolean',
+    'payment' => 'nullable|numeric',
+  ];
 
   /**
    * Show the form for creating a new resource.
    *
-   * @return \Illuminate\Http\Response
+   * @param  Show    $show
+   * @param  Request $request
+   * @return Response
    */
-  public function create()
+  public function create(Show $show, Request $request)
   {
-    //
+    // Authorize the request
+    /**
+     * The authorization is done via show because the show is what is required
+     * to create or remove these fields
+     */
+    $this->authorize('delete', $show);
+
+    // Return the view
+    return view('show.user.create', [
+      'show' => $show,
+      'users' => $request
+        ->user()
+        ->userCanSee()
+        ->withCount([
+          'shows' => function($query) use ($show) {
+            $query
+              ->where('shows.id', $show->id);
+          }
+        ])
+        ->orderBy('last_name', 'ASC')
+        ->orderBy('first_name', 'ASC')
+        ->get(),
+    ]);
   }
 
   /**
    * Store a newly created resource in storage.
    *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
+   * @param  Show    $show
+   * @param  Request $request
+   * @return Response
    */
-  public function store(Request $request)
+  public function store(Show $show, Request $request)
   {
-    //
-  }
+    // Authorize the request
+    $this->authorize('delete', $show);
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\ShowUser  $showUser
-   * @return \Illuminate\Http\Response
-   */
-  public function show(ShowUser $showUser)
-  {
-    //
+    // Add to the validation array
+    $this->rules['user_id'] = 'required|exists:users,id';
+
+    // Validate the request
+    $request->validate($this->rules);
+
+    // Get the user
+    $user = \App\User::find($request->input('user_id'));
+
+    // Check for a relationship
+    $relationship = $show
+      ->users
+      ->find($user);
+
+    if ($relationship !== null) {
+      return redirect()
+        ->route('show.user.edit', [
+          'show' => $show,
+          'user' => $user,
+        ])
+        ->with([
+          'message' => 'Relationship Already Exists',
+        ]);
+    }
+
+    // Make the association
+    $show
+      ->users()
+      ->save($user, [
+        'is_owner' => $request->input('is_owner'),
+        'is_driver' => $request->input('is_driver'),
+        'is_shooter' => $request->input('is_shooter'),
+        'is_assistant' => $request->input('is_assistant'),
+        'payment' => $request->input('payment'),
+      ]);
+
+    return redirect()
+      ->route('show.user.create', [
+        'show' => $show,
+      ])
+      ->with([
+        'message' => $user->first_name . ' ' . $user->last_name . ' was added',
+      ]);
   }
 
   /**
    * Show the form for editing the specified resource.
    *
-   * @param  \App\ShowUser  $showUser
-   * @return \Illuminate\Http\Response
+   * @param  Show   $show
+   * @param  User   $user
+   * @return Response
    */
-  public function edit(ShowUser $showUser)
+  public function edit(Show $show, User $user)
   {
-    //
+    // Authorize the request
+    $this->authorize('delete', $show);
+
+    // Get the user with pivot data
+    $userWithPivot = $show
+      ->users
+      ->find($user);
+
+    // If the relationship doesn't exist return a create screen
+    if ($userWithPivot === null) {
+      return redirect()
+        ->route('show.user.create', [
+          'show' => $show,
+        ])
+        ->withInput([
+          'user_id' => $user->id,
+        ])
+        ->with([
+          'message' => 'User must be added to the show',
+        ]);
+    }
+
+    return view('show.user.edit', [
+      'show' => $show,
+      'user' => $userWithPivot,
+    ]);
   }
 
   /**
    * Update the specified resource in storage.
    *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \App\ShowUser  $showUser
-   * @return \Illuminate\Http\Response
+   * @param  Request $request
+   * @param  Show    $show
+   * @param  User    $user
+   * @return Response
    */
-  public function update(Request $request, ShowUser $showUser)
+  public function update(Request $request, Show $show, User $user)
   {
-    //
+    // Authorize the request
+    $this->authorize('delete', $show);
+
+    // Validate the request
+    $request->validate($this->rules);
+
+    // Get the relationship
+    $relationship = $show
+      ->users
+      ->find($user)
+      ->pivot;
+
+    // Save the new values
+    $relationship->is_owner = $request->input('is_owner');
+    $relationship->is_driver = $request->input('is_driver');
+    $relationship->is_shooter = $request->input('is_shooter');
+    $relationship->is_assistant = $request->input('is_assistant');
+    $relationship->payment = $request->input('payment');
+    $relationship->save();
+
+    // Return to the show view
+    return redirect()
+      ->route('show.show', [
+        'show' => $show,
+      ])
+      ->with([
+        'message' => $user->first_name . ' ' . $user->last_name . ' Updated',
+      ]);
   }
 
   /**
    * Remove the specified resource from storage.
    *
-   * @param  \App\ShowUser  $showUser
-   * @return \Illuminate\Http\Response
+   * @param  Show   $show
+   * @param  User   $user
+   * @return Response
    */
-  public function destroy(ShowUser $showUser)
+  public function destroy(Show $show, User $user)
   {
-    //
+    // Authorize the request
+    $this->authorize('delete', $show);
+
+    // Delete the relationship
+    $relationship = $show
+      ->users
+      ->find($user)
+      ->delete();
+
+    // Return to the show view
+    return redirect()
+      ->route('show.show', [
+        'show' => $show,
+      ])
+      ->with([
+        'message' => $user->first_name . ' ' . $user->last_name . ' Removed',
+      ]);
   }
 }
