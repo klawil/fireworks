52ea9842066d6410fb4cad7d875a273a237d97cb
William Klausmeyer <willyk95@gmail.com>
2017-09-29 15:50:38 -0500

    Fill out the contact model

--- app/Contact.php
+++ app/Contact.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
-  //
+  use SoftDeletes;
+
+  /**
+   * The table associated with the model
+   *
+   * @var string
+   */
+  protected $table = 'contact';
+
+  /**
+   * The show the contact belongs to
+   */
+  public function show() {
+    return $this
+      ->belongsTo('App\Show');
+  }
 }
