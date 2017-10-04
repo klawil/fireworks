54bd71eec1ab7b8ba44eb0e325a193b7c3978d6b
William Klausmeyer <willyk95@gmail.com>
2017-10-04 15:48:53 -0500

    Update the contact forms to use the helpers

--- resources/views/show/contact/edit.blade.php
+++ resources/views/show/contact/edit.blade.php
@@ -12,61 +12,33 @@
           {{ csrf_field() }}
           {{ method_field('PUT') }}
 
-          <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-            <label for="name" class="col-md-4 control-label">Contact Name*</label>
+          @include('assets.input', [
+            'label' => 'Contact Name *',
+            'name' => 'name',
+            'default' => $contact->name,
+            'required' => true,
+            'autofocus' => true,
+          ])
 
-            <div class="col-md-6">
-              <input id="name" name="name" type="text" class="form-control" value="{{ old('name', $contact->name) }}" autofocus required>
+          @include('assets.input', [
+            'label' => 'Contact Description',
+            'name' => 'description',
+            'default' => $contact->name,
+          ])
 
-              @if ($errors->has('name'))
-              <span class="help-block">
-                <strong>{{ $errors->first('name') }}</strong>
-              </span>
-              @endif
-            </div>
-          </div>
+          @include('assets.input', [
+            'label' => 'Phone Number',
+            'name' => 'phone',
+            'default' => $contact->name,
+            'type' => 'tel',
+          ])
 
-          <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
-            <label for="description" class="col-md-4 control-label">Contact Description</label>
-
-            <div class="col-md-6">
-              <input id="description" name="description" type="text" class="form-control" value="{{ old('description', $contact->description) }}">
-
-              @if ($errors->has('description'))
-              <span class="help-block">
-                <strong>{{ $errors->first('description') }}</strong>
-              </span>
-              @endif
-            </div>
-          </div>
-
-          <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-            <label for="phone" class="col-md-4 control-label">Phone Number</label>
-
-            <div class="col-md-6">
-              <input id="phone" name="phone" type="text" class="form-control" value="{{ old('phone', $contact->phone) }}">
-
-              @if ($errors->has('phone'))
-              <span class="help-block">
-                <strong>{{ $errors->first('phone') }}</strong>
-              </span>
-              @endif
-            </div>
-          </div>
-
-          <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-            <label for="email" class="col-md-4 control-label">Email</label>
-
-            <div class="col-md-6">
-              <input id="email" name="email" type="email" class="form-control" value="{{ old('email', $contact->email) }}">
-
-              @if ($errors->has('email'))
-              <span class="help-block">
-                <strong>{{ $errors->first('email') }}</strong>
-              </span>
-              @endif
-            </div>
-          </div>
+          @include('assets.input', [
+            'label' => 'Email',
+            'name' => 'email',
+            'default' => $contact->name,
+            'type' => 'email',
+          ])
 
           <div class="form-group">
             <div class="col-md-6 col-md-offset-4">
