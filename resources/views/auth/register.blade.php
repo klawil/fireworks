037b4e747df538a11af5e597d43f842cd3477701
William Klausmeyer <willyk95@gmail.com>
2017-10-04 15:32:27 -0500

    Move the registration view to the new helper

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,132 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading text-center">
-                  <h4>Register</h4>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">
+          <h4>Register</h4>
+        </div>
+
+        <div class="panel-body">
+          <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+            {{ csrf_field() }}
+
+            @include('assets.input', [
+              'label' => 'Name *',
+              'name' => 'name',
+              'required' => true,
+              'autofocus' => true,
+            ])
+
+            @include('assets.input', [
+              'label' => 'E-Mail Address *',
+              'name' => 'email',
+              'type' => 'email',
+              'required' => true,
+            ])
+
+            @include('assets.input', [
+              'label' => 'Phone Number',
+              'name' => 'phone',
+              'type' => 'tel',
+            ])
+
+            @include('assets.input', [
+              'label' => 'Address',
+              'name' => 'address',
+              'placeholder' => 'Street Address',
+            ])
+
+            <div class="form-group">
+                <label for="city" class="col-md-4 control-label"></label>
+
+                <div class="col-md-3{{ $errors->has('city') ? ' has-error' : '' }}">
+                    <input id="city" type="string" class="form-control" name="city" placeholder="City" value="{{ old('city') }}">
+
+                    @if ($errors->has('city'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('city') }}</strong>
+                        </span>
+                    @endif
                 </div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+                <div class="col-md-3{{ $errors->has('state') ? ' has-error' : '' }}">
+                    @include('assets.state', [
+                      'required' => false,
+                      'default' => null,
+                    ])
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name *</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address *</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-                            <label for="phone" class="col-md-4 control-label">Phone Number</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="tel" class="form-control" name="phone" value="{{ old('phone') }}">
-
-                                @if ($errors->has('phone'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('phone') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
-                            <label for="address" class="col-md-4 control-label">Address</label>
-
-                            <div class="col-md-6">
-                                <input id="address" type="string" class="form-control" name="address" placeholder="Address" value="{{ old('address') }}">
-
-                                @if ($errors->has('address'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('address') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="city" class="col-md-4 control-label"></label>
-
-                            <div class="col-md-3{{ $errors->has('city') ? ' has-error' : '' }}">
-                                <input id="city" type="string" class="form-control" name="city" placeholder="City" value="{{ old('city') }}">
-
-                                @if ($errors->has('city'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('city') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-
-                            <div class="col-md-3{{ $errors->has('state') ? ' has-error' : '' }}">
-                                @include('assets.state', [
-                                  'required' => false,
-                                  'default' => null,
-                                ])
-
-                                @if ($errors->has('state'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('state') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password *</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password *</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary btn-block">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    @if ($errors->has('state'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('state') }}</strong>
+                        </span>
+                    @endif
                 </div>
             </div>
+
+            @include('assets.input', [
+              'label' => 'Password *',
+              'name' => 'password',
+              'type' => 'password',
+              'required' => true,
+            ])
+
+            @include('assets.input', [
+              'label' => 'Confirm Password *',
+              'name' => 'password-confirm',
+              'type' => 'password',
+              'required' => true,
+            ])
+
+            <div class="form-group">
+              <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="btn btn-primary btn-block">
+                  Register
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 @endsection
